509d908655a4d6784682b359e75b0df784ace794
nguyenthanhlongvn03 <nguyenthanhlongvn03@gmail.com>
2025-11-13 20:46:57 +0700

    add

---
+++ do_you_love_me.js
@@ -0,0 +1,8 @@
+function dungim() {
+    let dungim = document.getElementById("dungim");
+
+}
+function dichuyen(){
+    let dichuyen = document.getElementById("dichuyen");
+    Math.random()
+}
--- may_tinh.js
+++ may_tinh.js
@@ -1,14 +1,18 @@
-const display = document.getElementById('display');
-let khoi_dau = '0';
-function appendValue(value) {
-    if (khoi_dau === '0' && value !== '.' && value !== '0') {
-        khoi_dau = value;
+let hien_thi = document.getElementById("ket_qua");
+
+function input(value) {
+    if ( hien_thi.value === '0' ){
+        //hien_thi.value = '0';
+        hien_thi.value = value
     }
-    else if (khoi_dau === '0' && value === '0') {
-        return 0;
+    else{
+        hien_thi.value += value;
     }
-    else {
-        khoi_dau += value;
-    }
-    display.value = khoi_dau;
+}
+function result(){
+    //let result = eval(hien_thi.value)
+    hien_thi.value = eval(hien_thi.value);
+}
+function xoa(){
+    hien_thi.value = "0";
 }
